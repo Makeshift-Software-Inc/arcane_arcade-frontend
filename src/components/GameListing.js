da3ba902f00f860d4ef4929f27193c2968035a21
Herbert Joseph <techniquea2z@gmail.com>
2020-10-01 08:39:38 -0400

    Add Tippy popover

--- src/components/GameListing.js
+++ src/components/GameListing.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
+import './GameListing.scss'
 
 class GameListing extends React.Component {
   constructor(props) {
@@ -8,14 +11,51 @@ class GameListing extends React.Component {
     this.state = { listing: this.props.listing }
   }
 
+
+  popoverContent() {
+    debugger
+
+    let popoverContent = [];
+
+    const firstImg  = this.state.listing.images[1];
+    const secondImg = this.state.listing.images[2];
+
+    if (firstImg) popoverContent.push(<img src={firstImg} alt=""/>)
+    if (secondImg) popoverContent.push(<img src={secondImg}  alt=""/>)
+
+    if (this.props.listing.videos.length > 0) {
+
+    }
+
+    debugger
+    return popoverContent;
+  }
+
+
+
   render() {
     const coverImageSrc = this.state.listing.images[0];
     const imgAlt = `${this.state.listing.title} cover`;
-    return (
-      <div className="game-listing">
-        <img src={coverImageSrc}  alt={imgAlt} />
 
+    const popupContent = (
+      <div className="popover">
+        {this.popoverContent()}
       </div>
+    )
+
+    return (
+      <Tippy
+        content={popupContent}
+        interactive={true}
+        interactiveBorder={20}
+        delay={100}
+        arrow={true}
+        placement="auto"
+      >
+        <div className="game-listing">
+          <img src={coverImageSrc}  alt={imgAlt} />
+        </div>
+      </Tippy>
     )
   }
 }
