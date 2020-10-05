32f7f5310629daf40d745624176a14fc10483e61
Sasa Sekaric <sasasekaric@hotmail.com>
2020-10-05 07:45:01 +0200

    Adding direct to s3 upload

--- src/store/models/UploadedFile.js
+++ src/store/models/UploadedFile.js
@@ -1,5 +1,10 @@
-import { types } from "mobx-state-tree";
+import { types, flow } from "mobx-state-tree";
 import BaseUpdate from "./BaseUpdate";
+
+import Api from "../../services/Api";
+import axios from "axios";
+
+const CancelToken = axios.CancelToken;
 
 const UploadedFile = types
   .model("UploadedFile", {
@@ -9,10 +14,62 @@ const UploadedFile = types
     url: types.string,
     data: types.frozen(),
     isDragged: false,
+    loading: false,
+    source: types.frozen(),
   })
   .actions((self) => ({
+    afterCreate() {
+      self.source = CancelToken.source();
+      self.upload();
+    },
     setDragging(value) {
       self.isDragged = value;
+    },
+    presign: flow(function* presign() {
+      const params = {
+        filename: self.name,
+        type: self.type,
+      };
+
+      // get direct to s3 upload params
+      try {
+        const response = yield Api.get("/s3/params", { params });
+        return response.data;
+      } catch (e) {
+        console.log("PRESIGN ERROR", e);
+        return false;
+      }
+    }),
+    upload: flow(function* upload() {
+      self.loading = true;
+      const presignParams = yield self.presign();
+
+      if (!presignParams) {
+        self.loading = false;
+        return false;
+      }
+
+      try {
+        const response = yield axios({
+          method: presignParams.method,
+          url: presignParams.url,
+          data: presignParams.fields,
+          headers: presignParams.headers,
+          onUploadProgress: self.onUploadProgress,
+          cancelToken: self.source.token,
+        });
+
+        console.log(response);
+      } catch (e) {
+        debugger;
+        console.log("UPLOAD ERROR", e);
+      }
+    }),
+    cancelUpload() {
+      self.source.cancel("Upload canceled");
+    },
+    onUploadProgress(progressEvent) {
+      console.log(progressEvent);
     },
   }));
 
