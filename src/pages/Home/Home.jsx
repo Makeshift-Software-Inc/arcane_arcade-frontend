bcd684225c782b3169e98c68ed93a8c258749173
Herbert Joseph <techniquea2z@gmail.com>
2020-10-12 18:42:31 -0400

    Fix linter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import ReactPlayer from 'react-player';
@@ -28,6 +28,13 @@ const Home = () => {
     selectedGame, games, load, loading,
   } = useStore('games');
 
+  useEffect(() => {
+    load();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) return <Loading />;
+
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
@@ -36,12 +43,40 @@ const Home = () => {
     e.preventDefault();
   };
 
-  useEffect(() => {
-    load();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const exploreRef = createRef();
+  const exploreContent = createRef();
+  const discoverRef = createRef();
+  const discoverContent = createRef();
 
-  if (loading) return <Loading />;
+  const switchPanels = (e) => {
+    e.preventDefault();
+
+    const { target } = e;
+
+    let sibling;
+
+    if (target === exploreRef.current) {
+      sibling = target.parentElement.previousSibling;
+    } else {
+      sibling = target.parentElement.nextSibling;
+    }
+
+    if (!target.classList.contains('selected')) {
+      const link = sibling.querySelector('a');
+      link.classList.remove('selected');
+      target.classList.add('selected');
+
+      const explore = exploreContent.current;
+      const discover = discoverContent.current;
+      if (explore.classList.contains('is-hidden')) {
+        explore.classList.remove('is-hidden');
+        discover.classList.add('is-hidden');
+      } else {
+        explore.classList.add('is-hidden');
+        discover.classList.remove('is-hidden');
+      }
+    }
+  };
 
   return (
     <div className="App">
@@ -53,10 +88,27 @@ const Home = () => {
         <div>
           <div className="tabs">
             <div className="tab">
-              <a className="selected">Discover</a>
+              <a
+                role="link"
+                tabIndex={0}
+                className="selected"
+                ref={discoverRef}
+                onClick={switchPanels}
+                onKeyDown={switchPanels}
+              >
+                Discover
+              </a>
             </div>
             <div className="tab">
-              <a>Explore</a>
+              <a
+                role="link"
+                ref={exploreRef}
+                onClick={switchPanels}
+                onKeyDown={switchPanels}
+                tabIndex={0}
+              >
+                Explore
+              </a>
             </div>
           </div>
 
@@ -70,10 +122,27 @@ const Home = () => {
           <div className="row">
             <div className="tabs">
               <div className="tab">
-                <a className="selected">Discover</a>
+                <a
+                  className="selected"
+                  role="link"
+                  tabIndex={0}
+                  ref={discoverRef}
+                  onClick={switchPanels}
+                  onKeyDown={switchPanels}
+                >
+                  Discover
+                </a>
               </div>
               <div className="tab">
-                <a>Explore</a>
+                <a
+                  role="link"
+                  tabIndex={0}
+                  ref={exploreRef}
+                  onClick={switchPanels}
+                  onKeyDown={switchPanels}
+                >
+                  Explore
+                </a>
               </div>
             </div>
             <div className="slider">
@@ -130,70 +199,66 @@ const Home = () => {
       }
       </div>
 
-      <div className="new-releases">
-        <h1>New Releases</h1>
-        <Splide
-          className="new release-slider"
-          options={{
-            type: 'loop',
-            clones: 0,
-            perPage: 4,
-            perMove: 1,
-            width: 1000,
-            height: 250,
-            rewind: false,
-            gap: '0.1rem',
-            keyboard: false,
-            lazyLoad: true,
-          }}
-        >
-          {
-          games.map((game) => {
-            const imageAlt = `${game.title} cover`;
-            const listingShowLink = `/games/${game.slug}`;
+      <div className="discover" ref={discoverContent}>
+        <div className="new-releases">
+          <h1>New Releases</h1>
+          <Splide
+            className="new release-slider"
+            options={{
+              type: 'loop',
+              clones: 0,
+              perPage: 4,
+              perMove: 1,
+              width: 1000,
+              height: 250,
+              rewind: false,
+              gap: '0.1rem',
+              keyboard: false,
+              lazyLoad: true,
+            }}
+          >
+            {
+              games.map((game) => {
+                const imageAlt = `${game.title} cover`;
+                const listingShowLink = `/games/${game.slug}`;
 
-            return (
-              <SplideSlide key={game.id}>
-                <div className="game-listing" key={game.id}>
-                  <Link to={listingShowLink}>
-                    <img src={game.images[0]} alt={imageAlt} />
-                  </Link>
-                </div>
-              </SplideSlide>
-            );
-          })
-        }
-        </Splide>
-      </div>
+                return (
+                  <SplideSlide key={game.id}>
+                    <div className="game-listing" key={game.id}>
+                      <Link to={listingShowLink}>
+                        <img src={game.images[0]} alt={imageAlt} />
+                      </Link>
+                      <div className="magic foolishIn info" />
+                    </div>
+                  </SplideSlide>
+                );
+              })
+            }
+          </Splide>
+        </div>
 
-      <div className="promotions">
-        <h1>Promotions</h1>
+        <div className="promotions">
+          <h1>Promotions</h1>
 
-        <div className="games">
-          <div className="game-list">
-            <GamesListings />
+          <div className="games">
+            <div className="game-list">
+              <GamesListings />
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="is-hidden">
-        <nav className="navbar browse-listings" role="navigation" ariaLabel="main-navigation">
+      <div className="explore is-hidden" ref={exploreContent}>
+        <nav className="navbar browse-listings" role="navigation" aria-label="main-navigation">
           <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-start">
-              <div className="navbar-item">
-                <form onSubmit={handleSubmit}>
-                  <input onChange={handleSearchChange} value={searchQuery} type="search" placeholder="enter search term or tag" className="topcoat-search-input" />
-                </form>
-              </div>
-            </div>
 
-            <div className="navbar-end">
+            <div className="navbar-start">
               <div className="navbar-item">
                 <label htmlFor="sort-by">Sort By</label>
 
                 <div className="select">
                   <select name="sort-by">
-                    <option value="sort_by">Relevance</option>
+                    <option value="relevance">Relevance</option>
                     <option value="release_date">Release Date</option>
                     <option value="Name">Name</option>
                     <option value="price_asc">Lowest Price</option>
@@ -201,6 +266,22 @@ const Home = () => {
                     <option value="reviews">User Reviews</option>
                   </select>
                 </div>
+              </div>
+
+              <div className="navbar-item">
+                <label htmlFor="platform">Platform</label>
+
+                <select name="platform">
+                  <option value="PC">PC</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <form onSubmit={handleSubmit}>
+                  <input onChange={handleSearchChange} value={searchQuery} type="search" placeholder="enter search term or tag" className="topcoat-search-input" />
+                </form>
               </div>
             </div>
           </div>
