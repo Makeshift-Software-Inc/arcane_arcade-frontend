d787ab0fab12e45eb10c6e6330852e8ddbd252ff
Herbert Joseph <techniquea2z@gmail.com>
2020-10-12 15:33:10 -0400

    pix paragraph length

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import ReactPlayer from 'react-player';
 
-import {observer} from 'mobx-react';
+import { observer } from 'mobx-react';
 
-import {Splide, SplideSlide} from '@splidejs/react-splide';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 
 import './Home.scss';
 
@@ -20,11 +20,13 @@ import kingdomCome from '../../img/kingdom_come.jpeg';
 import greedfall from '../../img/greedfall.jpg';
 import xcom from '../../img/xcom.jpg';
 
-import {useStore} from '../../store';
+import { useStore } from '../../store';
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const {selectedGame, games, load, loading} = useStore('games');
+  const {
+    selectedGame, games, load, loading,
+  } = useStore('games');
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
@@ -39,15 +41,16 @@ const Home = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (loading)
-    return <Loading/>;
+  if (loading) return <Loading />;
 
-  return (<div className="App">
-    <Navbar/>
+  return (
+    <div className="App">
+      <Navbar />
 
-    <div className="slider-container">
-      {
-        selectedGame && (<div>
+      <div className="slider-container">
+        {
+        selectedGame && (
+        <div>
           <div className="tabs">
             <div className="tab">
               <a className="selected">Discover</a>
@@ -57,11 +60,13 @@ const Home = () => {
             </div>
           </div>
 
-          <ReactPlayer url={selectedGame.videos[0]} playing="playing" width="75vw" height="50vh" autoPlay="autoPlay" controls="controls" muted="muted"/>
-        </div>)
+          <ReactPlayer url={selectedGame.videos[0]} playing="playing" width="75vw" height="50vh" autoPlay="autoPlay" controls="controls" muted="muted" />
+        </div>
+        )
       }
-      {
-        !selectedGame && (<div className="flex">
+        {
+        !selectedGame && (
+        <div className="flex">
           <div className="row">
             <div className="tabs">
               <div className="tab">
@@ -72,27 +77,30 @@ const Home = () => {
               </div>
             </div>
             <div className="slider">
-              <Splide className="splide-slider" options={{
+              <Splide
+                className="splide-slider"
+                options={{
                   width: 600,
                   height: 450,
                   clones: 0,
                   lazyLoad: true,
-                  waitForTransition: true
-                }}>
+                  waitForTransition: true,
+                }}
+              >
                 <SplideSlide>
-                  <img src={kingdomCome} alt="kingdom come deliverance cover"/>
+                  <img src={kingdomCome} alt="kingdom come deliverance cover" />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src={fire_emblem} alt="civilizations 6 cover"/>
+                  <img src={fire_emblem} alt="civilizations 6 cover" />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src={hades} alt="hades cover"/>
+                  <img src={hades} alt="hades cover" />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src={greedfall} alt="greedfall 6 cover"/>
+                  <img src={greedfall} alt="greedfall 6 cover" />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src={xcom} alt="civilizations 6 cover"/>
+                  <img src={xcom} alt="civilizations 6 cover" />
                 </SplideSlide>
               </Splide>
             </div>
@@ -100,114 +108,126 @@ const Home = () => {
               <h1>Kingdom Come: Deliverance</h1>
 
               <p>
-                From its inception, Kingdom Come: Deliverance was billed as a game steeped in realism. From period-accurate food and weapon damage to characters drawn from history, Warhorse Studios did its homework. With the game’s release last week, we finally got to play in their (as promised) realistic version of 15th century Bohemia.
+                From its inception, Kingdom Come: Deliverance was billed as a
+                game steeped in realism. From period-accurate food and weapon
+                damage to characters drawn from history, Warhorse Studios did
+                its homework. With the game’s release last week, we finally got
+                to play in their (as promised) realistic version of
+                15th century Bohemia.
               </p>
 
               <div className="platform-icons">
                 <a>Learn More ⟶</a>
                 <div className="icons">
-                  <i className="fab fa-windows"/>
+                  <i className="fab fa-windows" />
                 </div>
               </div>
             </div>
 
           </div>
-        </div>)
+        </div>
+        )
       }
-    </div>
+      </div>
 
-    <div className="new-releases">
-      <h1>New Releases</h1>
-      <Splide className="new release-slider" options={{
-          type: 'loop',
-          clones: 0,
-          perPage: 4,
-          perMove: 1,
-          width: 1000,
-          height: 250,
-          rewind: false,
-          gap: '0.1rem',
-          keyboard: false,
-          lazyLoad: true
-        }}>
-        {
+      <div className="new-releases">
+        <h1>New Releases</h1>
+        <Splide
+          className="new release-slider"
+          options={{
+            type: 'loop',
+            clones: 0,
+            perPage: 4,
+            perMove: 1,
+            width: 1000,
+            height: 250,
+            rewind: false,
+            gap: '0.1rem',
+            keyboard: false,
+            lazyLoad: true,
+          }}
+        >
+          {
           games.map((game) => {
             const imageAlt = `${game.title} cover`;
             const listingShowLink = `/games/${game.slug}`;
 
-            return (<SplideSlide key={game.id}>
-              <div className="game-listing" key={game.id}>
-                <Link to={listingShowLink}>
-                  <img src={game.images[0]} alt={imageAlt}/>
-                </Link>
-              </div>
-            </SplideSlide>);
+            return (
+              <SplideSlide key={game.id}>
+                <div className="game-listing" key={game.id}>
+                  <Link to={listingShowLink}>
+                    <img src={game.images[0]} alt={imageAlt} />
+                  </Link>
+                </div>
+              </SplideSlide>
+            );
           })
         }
-      </Splide>
-    </div>
+        </Splide>
+      </div>
 
-    <div className="promotions">
-      <h1>Promotions</h1>
+      <div className="promotions">
+        <h1>Promotions</h1>
 
-      <div className="games">
-        <div className="game-list">
-          <GamesListings/>
+        <div className="games">
+          <div className="game-list">
+            <GamesListings />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="is-hidden">
-      <nav className="navbar browse-listings" role="navigation" ariaLabel="main-navigation">
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <div className="navbar-item">
-              <form onSubmit={handleSubmit}>
-                <input onChange={handleSearchChange} value={searchQuery} type="search" placeholder="enter search term or tag" className="topcoat-search-input"/>
-              </form>
+      <div className="is-hidden">
+        <nav className="navbar browse-listings" role="navigation" ariaLabel="main-navigation">
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <div className="navbar-item">
+                <form onSubmit={handleSubmit}>
+                  <input onChange={handleSearchChange} value={searchQuery} type="search" placeholder="enter search term or tag" className="topcoat-search-input" />
+                </form>
+              </div>
+            </div>
+
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <label htmlFor="sort-by">Sort By</label>
+
+                <div className="select">
+                  <select name="sort-by">
+                    <option value="sort_by">Relevance</option>
+                    <option value="release_date">Release Date</option>
+                    <option value="Name">Name</option>
+                    <option value="price_asc">Lowest Price</option>
+                    <option value="price_desc">Highest Price</option>
+                    <option value="reviews">User Reviews</option>
+                  </select>
+                </div>
+              </div>
             </div>
           </div>
+        </nav>
 
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <label htmlFor="sort-by">Sort By</label>
+        <div className="games">
+          <div className="game-list">
+            <GamesListings />
+          </div>
 
-              <div className="select">
-                <select name="sort-by">
-                  <option value="sort_by">Relevance</option>
-                  <option value="release_date">Release Date</option>
-                  <option value="Name">Name</option>
-                  <option value="price_asc">Lowest Price</option>
-                  <option value="price_desc">Highest Price</option>
-                  <option value="reviews">User Reviews</option>
-                </select>
+          <div className="filters">
+            <div className="card">
+              <header className="card-header">
+                <p className="card-header-title">Narrow by Price</p>
+              </header>
+              <div className="card-content">
+                <div className="content">
+                  <input type="range" className="topcoat-range" />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </nav>
-
-      <div className="games">
-        <div className="game-list">
-          <GamesListings/>
-        </div>
-
-        <div className="filters">
-          <div className="card">
-            <header className="card-header">
-              <p className="card-header-title">Narrow by Price</p>
-            </header>
-            <div className="card-content">
-              <div className="content">
-                <input type="range" className="topcoat-range"/>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
 
-  </div>);
+    </div>
+  );
 };
 
 export default observer(Home);
