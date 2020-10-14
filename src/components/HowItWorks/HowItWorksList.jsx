2f41f0f04d44c628c404db2632ac170fe5b70937
Herbert Joseph <techniquea2z@gmail.com>
2020-10-13 22:06:51 -0400

    Fix linter

--- src/components/HowItWorks/HowItWorksList.jsx
+++ src/components/HowItWorks/HowItWorksList.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import TextAndImg from "./TextAndImg"
+import React from 'react';
+import TextAndImg from './TextAndImg';
 
-const HowItWorksList = ({data}) => {
+const HowItWorksList = ({ data }) => (
 
-  return (
- 
-      data.map((item, i) => {
-        return <TextAndImg text={item.text} image={item.img} key={i} odd={i % 2 == 0 ? false : true}/>
-      })
+  data.map((item, i) => (
+    <TextAndImg
+      text={item.text}
+      image={item.img}
+      odd={i % 2 !== 0}
+    />
+  ))
 
-
-  )
-}
+);
 
 export default HowItWorksList;
--- src/components/HowItWorks/TextAndImg.jsx
+++ src/components/HowItWorks/TextAndImg.jsx
@@ -1,22 +1,19 @@
-import React from "react";
+import React from 'react';
 
 const TextAndImg = ({
-  image, text, odd
-}) => {
+  image, text, odd,
+}) => (
+  <div className={`flex flex-row justify-between image-and-text ${odd ? 'flex-row-reverse' : ''}`}>
 
-  return (
-    <div className={`flex flex-row justify-between image-and-text ${odd ? 'flex-row-reverse' : ''}`}>
-
-      <div className="image-div">
-        <img src={image} alt="div-img" className="image" />
-      </div>
-
-      <div className="text-div flex align-center justify-flex-start">
-        <p  className="text" >{text}</p>
-      </div>
-
+    <div className="image-div">
+      <img src={image} alt="div-img" className="image" />
     </div>
-  )
-}
+
+    <div className="text-div flex align-center justify-flex-start">
+      <p className="text">{text}</p>
+    </div>
+
+  </div>
+);
 
 export default TextAndImg;
