ce262a7c1d814513c5ae0762d05237720ff6cc88
jovana <toskovic.jovana@gmail.com>
2020-10-19 23:20:38 +0200

    Add design for video and images slider. Add top bar with back button and SearchInput component.

--- src/pages/Games/Show/GamesShow.jsx
+++ src/pages/Games/Show/GamesShow.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 import { toast } from 'react-toastify';
 
+import { withRouter } from 'react-router-dom';
+
 import ReactPlayer from 'react-player';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 
@@ -15,10 +17,15 @@ import 'trix/dist/trix.css';
 
 import { useStore } from '../../../store';
 
+import backSvg from '../../../img/back.svg'
+
 import Navbar from '../../../components/Navbar/Navbar';
 import Loading from '../../../components/Loading/Loading';
 import BuyModal from './Buy/Modal';
 import OrderDetailsModal from '../../MyLibrary/Modal/Modal';
+
+import SearchInput from '../../../components/Form/SearchInput/SearchInput';
+
 
 import './GamesShow.scss';
 
@@ -32,8 +39,9 @@ const Videos = ({ videos, thumbnail }) => videos.map((video) => (
   <SplideSlide key={video}>
     <ReactPlayer
       url={video}
-      thumbnail={thumbnail}
+      light={thumbnail}
       playing={false}
+      preload={thumbnail}
       controls
       muted
     />
@@ -85,118 +93,135 @@ const GamesShow = ({ match, history }) => {
     reset();
   };
 
+  const TopSearchBar = () => (
+    <div className="flex-row justify-between top-search-bar">
+      <div className="flex-row align-center back-button" onClick={() => history.goBack()}> 
+        <img src={backSvg} alt="back-button" className="back-img"/>
+        <p>Back to store</p>
+      </div>
+
+      <div className="flex-row">
+        <SearchInput />
+      </div>
+    </div>
+  );
+
   return (
     <div className="App listings-show">
       <Navbar />
+      <div className="game-page-container">
 
-      <div className="splide-container">
-        <Splide
-          className="splide-slider"
-          options={{
-            clones: 0,
-            lazyLoad: true,
-            waitForTransition: true,
-            perPage: 1,
-          }}
-        >
-          <Splides
-            images={selectedGame.images}
-            videos={selectedGame.videos}
-            gameTitle={selectedGame.title}
-          />
-        </Splide>
-      </div>
+        <TopSearchBar />
 
-      <div className="info-section">
-        <div className="pricing">
-          <form>
-            <div className="payment">
-              <div className="crypto">
-                {selectedGame.accepts_bitcoin && (
-                  <div className="bitcoin">
-                    <label className="topcoat-radio-button">
-                      <Tippy
-                        content={`${selectedGame.btc_amount} BTC`}
-                        interactive
-                        interactiveBorder={20}
-                        delay={100}
-                        arrow
-                        placement="auto"
-                      >
-                        <i className="fab fa-bitcoin" />
-                      </Tippy>
-                    </label>
-                  </div>
-                )}
-                {selectedGame.accepts_monero && (
-                  <div className="monero">
-                    <label className="topcoat-radio-button">
-                      <Tippy
-                        content={`${selectedGame.xmr_amount} XMR`}
-                        interactive
-                        interactiveBorder={20}
-                        delay={100}
-                        arrow
-                        placement="auto"
-                      >
-                        <i className="fab fa-monero" />
-                      </Tippy>
-                    </label>
-                  </div>
-                )}
-              </div>
-
-              <div className="fiat">
-                {selectedGame.price && (
-                  <h3>
-                    {selectedGame.currency_symbol}
-                    {selectedGame.price}
-                    {' '}
-                    {selectedGame.default_currency}
-                  </h3>
-                )}
-              </div>
-            </div>
-
-            <div className="vl" />
-
-            <div className="platforms">
-              {selectedGame.hasSupportedPlatform('WINDOWS') && (
-                <div className="windows">
-                  <i className="fab fa-windows" />
-                  <h3>Windows</h3>
-                </div>
-              )}
-
-              {selectedGame.hasSupportedPlatform('MAC') && (
-                <div className="mac">
-                  <i className="fab fa-apple" />
-                  <h3>Mac</h3>
-                </div>
-              )}
-
-              {selectedGame.hasSupportedPlatform('LINUX') && (
-                <div className="linux">
-                  <i className="fab fa-linux" />
-                  <h3>Linux</h3>
-                </div>
-              )}
-            </div>
-            <div className="payment-submit">
-              <button onClick={openBuyModal} className="button" type="button">
-                BUY NOW
-              </button>
-            </div>
-          </form>
+        <div className="splide-container">
+          <Splide
+            className="splide-slider"
+            options={{
+              clones: 0,
+              lazyLoad: true,
+              waitForTransition: true,
+              perPage: 1,
+            }}
+          >
+            <Splides
+              images={selectedGame.images}
+              videos={selectedGame.videos}
+              gameTitle={selectedGame.title}
+            />
+          </Splide>
         </div>
-        <div
-          className="description"
-          // eslint-disable-next-line
-          dangerouslySetInnerHTML={{ __html: selectedGame.description }}
-        />
+
+        <div className="info-section">
+          <div className="pricing">
+            <form>
+              <div className="payment">
+                <div className="crypto">
+                  {selectedGame.accepts_bitcoin && (
+                    <div className="bitcoin">
+                      <label className="topcoat-radio-button">
+                        <Tippy
+                          content={`${selectedGame.btc_amount} BTC`}
+                          interactive
+                          interactiveBorder={20}
+                          delay={100}
+                          arrow
+                          placement="auto"
+                        >
+                          <i className="fab fa-bitcoin" />
+                        </Tippy>
+                      </label>
+                    </div>
+                  )}
+                  {selectedGame.accepts_monero && (
+                    <div className="monero">
+                      <label className="topcoat-radio-button">
+                        <Tippy
+                          content={`${selectedGame.xmr_amount} XMR`}
+                          interactive
+                          interactiveBorder={20}
+                          delay={100}
+                          arrow
+                          placement="auto"
+                        >
+                          <i className="fab fa-monero" />
+                        </Tippy>
+                      </label>
+                    </div>
+                  )}
+                </div>
+
+                <div className="fiat">
+                  {selectedGame.price && (
+                    <h3>
+                      {selectedGame.currency_symbol}
+                      {selectedGame.price}
+                      {' '}
+                      {selectedGame.default_currency}
+                    </h3>
+                  )}
+                </div>
+              </div>
+
+              <div className="vl" />
+
+              <div className="platforms">
+                {selectedGame.hasSupportedPlatform('WINDOWS') && (
+                  <div className="windows">
+                    <i className="fab fa-windows" />
+                    <h3>Windows</h3>
+                  </div>
+                )}
+
+                {selectedGame.hasSupportedPlatform('MAC') && (
+                  <div className="mac">
+                    <i className="fab fa-apple" />
+                    <h3>Mac</h3>
+                  </div>
+                )}
+
+                {selectedGame.hasSupportedPlatform('LINUX') && (
+                  <div className="linux">
+                    <i className="fab fa-linux" />
+                    <h3>Linux</h3>
+                  </div>
+                )}
+              </div>
+              <div className="payment-submit">
+                <button onClick={openBuyModal} className="button" type="button">
+                  BUY NOW
+                </button>
+              </div>
+            </form>
+          </div>
+          <div
+            className="description"
+            // eslint-disable-next-line
+            dangerouslySetInnerHTML={{ __html: selectedGame.description }}
+          />
+        </div>
+        {showBuyModal && <BuyModal close={closeBuyModal} />}
+        <OrderDetailsModal />
       </div>
-      {showBuyModal && <BuyModal close={closeBuyModal} />}
-      <OrderDetailsModal />
     </div>
   );
 };
