470f3ef00933114c277fd7b747511c8fcdd54d17
Herbert Joseph <techniquea2z@gmail.com>
2020-10-28 21:14:58 -0400

    linux icon

--- src/pages/Games/Show/Buy/Platform/Platform.jsx
+++ src/pages/Games/Show/Buy/Platform/Platform.jsx
@@ -5,12 +5,14 @@ import Select from '../Select/Select';
 import windowsIcon from '../../../../../img/platform_icons/WINDOWS_blue.svg';
 import macIcon from '../../../../../img/platform_icons/MAC_blue.svg';
 import xb1Icon from '../../../../../img/platform_icons/XB1_blue.svg';
+import linuxIcon from '../../../../../img/platform_icons/linux_blue.svg';
 import switchIcon from '../../../../../img/platform_icons/SWITCH_blue.svg';
 import ps4Icon from '../../../../../img/platform_icons/PS4_blue.svg';
 
 const ICONS = {
   WINDOWS: windowsIcon,
   MAC: macIcon,
+  LINUX: linuxIcon,
   SWITCH: switchIcon,
   PS4: ps4Icon,
   XB1: xb1Icon,
