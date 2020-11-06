a4debf0c073cfc1dfbcda31c68c3fb32e5af7021
jovana <toskovic.jovana@gmail.com>
2020-11-06 22:09:24 +0100

    Add sell-with-us page.

---
+++ src/pages/SellWithUs/SellWithUs.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import Navbar from './../../components/Navbar/Navbar';
+
+
+
+const SellWithUs = () => {
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <p>Sell with us</p>
+      </div>
+    </div>
+  )
+}
+
+export default SellWithUs;
